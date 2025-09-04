ac62ea9405382a162d4c0142e77d526a4715a4c0
sornersbooms <oscardanielyepez777@gmail.com>
2025-09-04 11:00:57 -0500

    nuevo commit mas4

--- Frontend/src/app/noticias/page.js
+++ Frontend/src/app/noticias/page.js
@@ -6,7 +6,7 @@ import { getRandomLocalImage } from '../../utils/imageUtils';
 async function getNews() {
   console.log('Executing getNews()');
   try {
-    const res = await fetch(`${process.env.API_URL}/api/news`, { next: { revalidate: 3600 } }); // Revalida cada hora
+    const res = await fetch(`https://cryptomonedashoy-production.up.railway.app/api/news`, { cache: 'no-store' }); // Deshabilita la caché
     if (!res.ok) {
       throw new Error('Failed to fetch news');
     }
